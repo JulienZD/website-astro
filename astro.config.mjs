f7c8016db9a80f459e2f74ea938ef5cc63c6556a
Julien Zapata Duque <jzapata@live.nl>
2023-06-27 14:27:36 +0200

    feat: optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
 import { addBlankTargets } from './src/lib/remark/blank-target';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
   markdown: {
     remarkPlugins: [addBlankTargets],
   },
